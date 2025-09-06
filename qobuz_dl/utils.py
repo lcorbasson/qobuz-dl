8de1294d334d997dd57b51f39bb33ac18a32b9e2
Loïc CORBASSON <loic.devel@corbasson.fr>
2025-09-06 10:43:59 +0200

    Fix failure on album names starting with parens/brackets

--- qobuz_dl/utils.py
+++ qobuz_dl/utils.py
@@ -115,8 +115,10 @@ def smart_discography_filter(
         Used to group two albums that may be named similarly, but not exactly
         the same.
         """
-        r = re.match(r"([^\(]+)(?:\s*[\(\[][^\)][\)\]])*", album)
-        return r.group(1).strip().lower()
+        r = album
+        r = re.sub(r"\([^\)]*\)", "", r)
+        r = re.sub(r"\[[^\]]*\)", "", r)
+        return r.strip().lower()
 
     requested_artist = contents[0]["name"]
     items = [item["albums"]["items"] for item in contents][0]
